f19ac1b6b952502b08075fe7eb1b0dbe27af8e5d
astoneallen <61259983+astoneallen@users.noreply.github.com>
2020-02-20 15:23:55 -0500

    Updated plot, commented out the xlab and ylab

--- src/plot_data.R
+++ src/plot_data.R
@@ -6,8 +6,8 @@ dev.off()
 # Plot the points out:
 ggplot(gmpts, aes(longitude, latitude, group = status))+
   scale_fill_gradient(low = "grey70", high = "grey30", guide = "none") +
-  xlab("Longitude") +
-  ylab("Latitude") +
+  # xlab("Longitude") +
+  # ylab("Latitude") +
   theme_minimal()+
   facet_grid(~ status, labeller = label_value) +
   stat_density_2d(aes(fill = ..level..), geom = "polygon", color = "black", size = 0.2, alpha = 0.5) +
